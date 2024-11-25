be16bb4ad722c13a750b426bfbd387825f33636b
Diksha <yogeshgadhe18@gmail.com>
2024-11-25 21:34:01 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript for Quantity Increment/Decrement
+document.getElementById('increment').addEventListener('click', function () {
+    const quantityInput = document.getElementById('quantity');
+    let quantity = parseInt(quantityInput.value);
+    quantityInput.value = quantity + 1;
+  });
+  
+  document.getElementById('decrement').addEventListener('click', function () {
+    const quantityInput = document.getElementById('quantity');
+    let quantity = parseInt(quantityInput.value);
+    if (quantity > 1) {
+      quantityInput.value = quantity - 1;
+    }
+  });
+  
